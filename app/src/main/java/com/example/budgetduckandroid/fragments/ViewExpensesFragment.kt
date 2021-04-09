1fc511062efd0c27e0d582313259f195dcbd0261
butlerr95 <reece.butler@hotmail.co.uk>
2021-04-09 20:22:17 +0100

    Added base code for fragment kotlin classes

--- app/src/main/java/com/example/budgetduckandroid/fragments/ViewExpensesFragment.kt
+++ app/src/main/java/com/example/budgetduckandroid/fragments/ViewExpensesFragment.kt
@@ -1,4 +1,33 @@
 package com.example.budgetduckandroid.fragments
 
-class ViewExpensesFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.budgetduckandroid.R
+import com.example.budgetduckandroid.databinding.FragmentViewExpensesBinding
+
+class ViewExpensesFragment : Fragment() {
+
+    private lateinit var binding: FragmentViewExpensesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_view_expenses,
+            container,
+            false
+        )
+
+        binding.lifecycleOwner = this
+
+        return binding.root
+    }
 }
